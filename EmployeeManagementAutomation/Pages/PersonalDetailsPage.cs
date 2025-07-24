63b2f22e8d619e3306cdc4457afd299e68068052
nagarani2092 <nagarani2092@gmail.com>
2025-07-24 18:30:59 +0530

    Added new pages

---
+++ EmployeeManagementAutomation/Pages/PersonalDetailsPage.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagementAutomation.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementAutomation.Pages
+{
+    public class PersonalDetailsPage : WebDriverKeywords
+    {
+        private IWebDriver _driver;
+
+        public PersonalDetailsPage(IWebDriver driver) : base(driver)
+        {
+            this._driver = driver;
+        }
+    }
+}
